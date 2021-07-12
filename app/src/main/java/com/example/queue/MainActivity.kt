5c770c3996c158ceb099b494f4234236d106e011
Shubham Garg <shubhamgarg1671@gmail.com>
2021-07-12 19:00:06 +0530

    fronted 1 tab_1

--- app/src/main/java/com/example/queue/MainActivity.kt
+++ app/src/main/java/com/example/queue/MainActivity.kt
@@ -3,7 +3,12 @@ package com.example.queue
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.viewpager.widget.ViewPager
+import com.example.queue.adapter.viewPagerAdapter
+import com.google.android.material.tabs.TabItem
+import com.google.android.material.tabs.TabLayout
 import com.google.firebase.auth.FirebaseAuth
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     val TAG = "MainActivity"
@@ -13,5 +18,34 @@ class MainActivity : AppCompatActivity() {
         Log.d(TAG, "onCreate() called with: savedInstanceState = $savedInstanceState")
 //        FirebaseAuth.getInstance().signOut()
 
+        val viewPager = findViewById<ViewPager>(R.id.viewPager);
+        val viewPagerAdapter = viewPagerAdapter(
+            supportFragmentManager
+        );
+        viewPager.setAdapter(viewPagerAdapter);
+        val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
+
+        // It is used to join TabLayout with ViewPager.
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                // Handle tab select
+
+                tab?.contentDescription = "shubham"
+                Log.d(TAG, "onTabSelected() called with: tab = ${tab!!.text}")
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                // Handle tab reselect
+                Log.d(TAG, "onTabReselected() called with: tab = $tab")
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                // Handle tab unselect
+                Log.d(TAG, "onTabUnselected() called with: tab = $tab")
+            }
+        })
     }
 }
---
+++ app/src/main/java/com/example/queue/adapter/viewPagerAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.queue.adapter
+
+import android.content.Context
+import android.content.res.Resources
+import android.provider.Settings.Global.getString
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.queue.R
+import com.example.queue.fragment.tab_1
+import com.example.queue.fragment.tab_2
+
+
+class viewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    val TAG = "viewPagerAdapter"
+    override fun getCount(): Int {
+        return 2;
+    }
+
+    override fun getItem(position: Int): Fragment {
+        var fragment: Fragment? = null
+        if (position == 0) fragment = tab_1()
+        else if (position == 1) fragment = tab_2()
+
+        return fragment!!
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        var title: String? = null
+        if (position == 0)
+            title = "Move in Queue"
+        else if (position == 1)
+            title  = "New Queue"
+        return title
+    }
+}
