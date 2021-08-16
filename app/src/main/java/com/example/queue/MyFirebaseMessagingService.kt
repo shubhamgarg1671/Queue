e148689b3a70f168390f59989120fff4f9d0818f
Shubham Garg <shubhamgarg1671@gmail.com>
2021-08-16 17:37:51 +0530

    notification setup when app in foreground

--- app/src/main/java/com/example/queue/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/queue/MyFirebaseMessagingService.kt
@@ -1,11 +1,44 @@
 package com.example.queue
 
+import android.R
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
+import android.os.Build
 import android.util.Log
-import com.google.firebase.messaging.FirebaseMessaging
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
 
 class MyFirebaseMessagingService :FirebaseMessagingService() {
     val TAG = "MyFirebaseMessagingServ"
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+        Log.d(TAG, "onMessageReceived: " + remoteMessage.data["message"])
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+        val channelId = "Default"
+        val builder: NotificationCompat.Builder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.mipmap.sym_def_app_icon)
+            .setContentTitle(remoteMessage.notification!!.title)
+            .setContentText(remoteMessage.notification!!.body).setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+        val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                "Default channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            manager.createNotificationChannel(channel)
+        }
+        manager.notify(0, builder.build())
+    }
+
     override fun onNewToken(p0: String) {
         Log.d(TAG, "onNewToken() called with: p0 = $p0")
     }
